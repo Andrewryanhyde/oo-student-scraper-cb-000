137abf05372cd3248be612bea77b8b6850dd7236
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-04 08:00:23 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,19 @@
 require 'open-uri'
+require 'Nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css('.student-name .student-location div.student-card a')
   end
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
     
   end
+
 
 end
 
