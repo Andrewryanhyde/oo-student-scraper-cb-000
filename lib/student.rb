12c50616ad9f24361615de600d80790b829fcf64
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-04 09:15:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,11 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each do |key, value| 
+      if self[key] == false 
+        self[key] = value
+      end
+    end
   end
 
   def self.all
