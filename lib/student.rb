2b10db47b13334b350e6513731134c31e6ae1a35
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-04 09:09:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,9 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |hash|
-      hash.each do Student.new(hash)
+      Student.new(hash)
+    end
+      
   end
 
   def add_student_attributes(attributes_hash)
